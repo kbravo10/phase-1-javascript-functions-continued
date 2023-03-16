5c06321f56148a72cc71b7ac040d8414172318d4
Kevin Bravo <kbravo1024@gmail.com>
2023-03-15 19:39:05 -0700

    complete functionsCont. lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(object1 = "roller-skate"){
+    return `This Saturday, I want to ${object1}!`;
+}
+
+function mondayWork(object2 = "go to the office"){
+    return `This Monday, I will ${object2}.`;
+}
+
+function wrapAdjective(special){
+    return function(msg){
+        return `You are ${special}${msg}${special}!`;
+    }
+}
